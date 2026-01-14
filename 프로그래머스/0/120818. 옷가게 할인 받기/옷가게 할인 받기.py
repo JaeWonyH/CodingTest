16320d046d508449cc3194b00a429aee2550b439
JAEWON HWANG <89454575+JaeWonyH@users.noreply.github.com>
2026-01-14 20:38:11 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 9.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,12 @@
+def solution(price):
+    answer = 0
+    if price < 100000:
+        answer = price
+    elif 300000 > price >= 100000:
+        answer = price * 0.95
+    elif 500000 > price >= 300000:
+        answer = price * 0.9
+    elif price >=500000:
+        answer = price * 0.8
+
+    return int(answer)
